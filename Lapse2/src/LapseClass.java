466814184269d486c1b987d5a101c427417f0eaa
William <William@WilliamPC>
2015-12-11 19:33:13 +0100

    first

---
+++ Lapse2/src/LapseClass.java
@@ -0,0 +1,18 @@
+
+public class LapseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Forsøg 1");
+		System.out.println("Forsøg 2");
+		System.out.println("Forsøg 3");
+		System.out.println("Forsøg 4");
+		System.out.println("Forsøg 5");
+		System.out.println("Forsøg 6");
+		System.out.println("Forsøg 7");
+		
+	}
+
+}
